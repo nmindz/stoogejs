6eecea5c3f56b2fad3a8361bd0bcae76c9c9633e
Evandro Camargo <nmindz@users.noreply.github.com>
2021-08-17 02:02:21 -0300

    define UserFactory and mock UserController

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,24 +1,21 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
 
 export default class UsersController {
-  public async index ({}: HttpContextContract) {
+  public async index({}: HttpContextContract) {
+    const user = await User.all()
+    return user
   }
 
-  public async create ({}: HttpContextContract) {
-  }
+  public async create({}: HttpContextContract) {}
 
-  public async store ({}: HttpContextContract) {
-  }
+  public async store({}: HttpContextContract) {}
 
-  public async show ({}: HttpContextContract) {
-  }
+  public async show({}: HttpContextContract) {}
 
-  public async edit ({}: HttpContextContract) {
-  }
+  public async edit({}: HttpContextContract) {}
 
-  public async update ({}: HttpContextContract) {
-  }
+  public async update({}: HttpContextContract) {}
 
-  public async destroy ({}: HttpContextContract) {
-  }
+  public async destroy({}: HttpContextContract) {}
 }
--- database/factories/index.ts
+++ database/factories/index.ts
@@ -1,1 +1,17 @@
 // import Factory from '@ioc:Adonis/Lucid/Factory'
+
+import User from 'App/Models/User'
+import Factory from '@ioc:Adonis/Lucid/Factory'
+
+export const UserFactory = Factory.define(User, ({ faker }) => {
+  return {
+    username: faker.internet.userName(),
+    display_name: faker.name.findName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    location:
+      faker.address.cityName() + ', ' + faker.address.stateAbbr() + ' - ' + faker.address.country(),
+    avatar_url: faker.image.avatar(),
+    bio: faker.lorem.sentences(3),
+  }
+}).build()
